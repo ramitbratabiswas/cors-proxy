8a28d30d034c09b32c7a1b5d34cd30906473116b
Ramit Brata Biswas <ramitbratabiswas@gmail.com>
2024-07-10 23:55:42 -0700

    completed scrape endpoint

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
 import fetch from 'node-fetch';
+import * as cheerio from 'cheerio';
 
 dotenv.config();
 
@@ -24,12 +25,48 @@ app.get('/api/lyrics', async (req, res) => {
 
   fetch(apiUrl).then(res => {
     return res.json();
+
   }).then(data => {
     if (data.message.body.lyrics) {
       let returned = data.message.body.lyrics.lyrics_body.split("***")[0];
       res.json({ lyrics: returned });
     }
   }).catch(error => error);
+});
+
+app.get('/api/scrape', async (req, res) => {
+  const { song, artist } = req.query;
+
+  const apiUrl = `https://www.musixmatch.com/lyrics/${artist}/${song}`;
+
+  console.log("scraping")
+  console.log(song + ", " + artist);
+
+  try {
+    const page = await fetch(apiUrl, {
+      method: "GET",
+    });
+
+    if (!page.ok) {
+      res.json({ lyrics: "sorry, we couldn't find the lyrics for this one :(" });
+      return;
+    }
+
+    const body = await page.text();
+    const $ = await cheerio.load(body);
+
+    const $lyrics = $('.r-ueyrd6');
+    const lyrics = $lyrics.map((i, e) => $(e).text().toString());
+    let finalLyrics = "";
+    for (let lyric of lyrics) {
+      finalLyrics += `${lyric}\n`;
+    }
+    console.log(finalLyrics);
+    return res.json({ lyrics: finalLyrics });
+  } catch (error) {
+    console.error(`catch clause error in server: ${error}`);
+  }
+
 });
 
 app.listen(port, () => {
